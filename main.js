b45549009da6c83525672e205b995e760a0dbb68
Rex <rex.ky.ng@gmail.com>
2020-07-30 12:34:49 +0800

    Revert "Revert "pepega fix for firefox on linux""
    This reverts commit 6fe1b1c7cf2a27cba661875b69f567b0203a8abb.

--- main.js
+++ main.js
@@ -1,71 +1,103 @@
 function displayDate() {
-    now = new Date();
-    switch(now.getDay()){
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-    }
-    switch(now.getMonth()){
-        case 0:
-            month = "January";
-            break;
-        case 1:
-            month = "February";
-            break;
-        case 2:
-            month = "March";
-            break;
-        case 3:
-            month = "April";
-            break;
-        case 4:
-            month = "May";
-            break;
-        case 5:
-            month = "June";
-            break;
-        case 6:
-            month = "July";
-            break;
-        case 7:
-            month = "August";
-            break;
-        case 8:
-            month = "September";
-            break;
-        case 9:
-            month = "October";
-            break;
-        case 10:
-            month = "November";
-            break;
-        case 11:
-            month = "December";
-    }
-    date = now.getDate();
-    year = now.getFullYear();
-    document.getElementById("clock").innerHTML = day+", "+month+" "+date+", "+year+" "+displayClock()+" HKT";
+  now = new Date();
+  switch (now.getDay()) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+  }
+  switch (now.getMonth()) {
+    case 0:
+      month = "January";
+      break;
+    case 1:
+      month = "February";
+      break;
+    case 2:
+      month = "March";
+      break;
+    case 3:
+      month = "April";
+      break;
+    case 4:
+      month = "May";
+      break;
+    case 5:
+      month = "June";
+      break;
+    case 6:
+      month = "July";
+      break;
+    case 7:
+      month = "August";
+      break;
+    case 8:
+      month = "September";
+      break;
+    case 9:
+      month = "October";
+      break;
+    case 10:
+      month = "November";
+      break;
+    case 11:
+      month = "December";
+  }
+  date = now.getDate();
+  year = now.getFullYear();
+  document.getElementById("clock").innerHTML =
+    day +
+    ", " +
+    month +
+    " " +
+    date +
+    ", " +
+    year +
+    " " +
+    displayClock() +
+    " HKT";
 }
 function displayClock() {
-	return (now.getHours() < 10 ? "0"+now.getHours() : now.getHours())+":"+(now.getMinutes() < 10 ? "0"+now.getMinutes() : now.getMinutes())+":"+(now.getSeconds() < 10 ? "0"+now.getSeconds() : now.getSeconds());
+  return (
+    (now.getHours() < 10 ? "0" + now.getHours() : now.getHours()) +
+    ":" +
+    (now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes()) +
+    ":" +
+    (now.getSeconds() < 10 ? "0" + now.getSeconds() : now.getSeconds())
+  );
 }
 
+function change_favicon(img) {
+  var favicon = document.querySelector('link[rel="shortcut icon"]');
+
+  if (!favicon) {
+    favicon = document.createElement("link");
+    favicon.setAttribute("rel", "shortcut icon");
+    var head = document.querySelector("head");
+    head.appendChild(favicon);
+  }
+
+  favicon.setAttribute("type", "image/png");
+  favicon.setAttribute("href", img);
+}
+
+window.location.replace("app://xdotool key ctrl+a");
+change_favicon("favicon-32x32.png");
 displayDate();
 setInterval(displayDate, 1000);
