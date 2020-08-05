ed4f4d3117405fe93213aca9c0a3685c4d7e52cd
Rex <rex.ky.ng@gmail.com>
2020-08-05 10:33:41 +0800

    firefox fix no longer needed with the latest nto

--- main.js
+++ main.js
@@ -97,9 +97,5 @@ function change_favicon(img) {
   favicon.setAttribute("href", img);
 }
 
-if (true) { //edit true to disable it
-  window.location.replace("app://xdotool key ctrl+a");
-  change_favicon("favicon-32x32.png");
-}
 displayDate();
 setInterval(displayDate, 1000);
