4866e9a969cc63d4d3ded44d922c4d29ade35cd5
timescam <rex.ky.ng@gmail.com>
2021-05-29 13:15:08 +0800

    tabs instead of space

--- README.md
+++ README.md
@@ -14,36 +14,36 @@ Old version is under legacy branch and will not be maintained anymore.
 <?xml version="1.0" encoding="UTF-8"?>
 <config>
 
-  <clock> <!-- do not change this -->
-    <timezone>HKT</timezone> <!--visual change only, can be leave blank -->
-    <url>https://calendar.google.com/calendar/</url> <!-- shortcut on the clock -->
-  </clock>
+	<clock> <!-- do not change this -->
+		<timezone>HKT</timezone> <!--visual change only, can be leave blank -->
+		<url>https://calendar.google.com/calendar/</url> <!-- shortcut on the clock -->
+	</clock>
 
-  <shortcuts> <!-- do not change this -->
-    <reddit> <!-- category name displayed as a folder in ls-->
-      <title>AyyMD</title> <!-- shortcut name displayed as a file in ls-->
-      <url>https://www.reddit.com/r/AyyMD/</url> <!-- url for shortcut -->
-    </reddit>
-    <reddit> <!-- add another block for another url in the same category -->
-      <title>COW</title>
-      <url>https://www.reddit.com/r/Competitiveoverwatch/</url>
-    </reddit>
+	<shortcuts> <!-- do not change this -->
+		<reddit> <!-- category name displayed as a folder in ls-->
+			<title>AyyMD</title> <!-- shortcut name displayed as a file in ls-->
+			<url>https://www.reddit.com/r/AyyMD/</url> <!-- url for shortcut -->
+		</reddit>
+		<reddit> <!-- add another block for another url in the same category -->
+			<title>COW</title>
+			<url>https://www.reddit.com/r/Competitiveoverwatch/</url>
+		</reddit>
 
-    <social> <!-- new category -->
-      <title>Discord</title>
-      <url>https://discordapp.com/app</url>
-    </social>
+		<social> <!-- new category -->
+			<title>Discord</title>
+			<url>https://discordapp.com/app</url>
+		</social>
 
-    <ssh>
-      <title>desktop</title>
-      <url>ssh://0.0.0.0</url> <!-- works with all kinds of url -->
-    </ssh>
+		<ssh>
+			<title>desktop</title>
+			<url>ssh://0.0.0.0</url> <!-- works with all kinds of url -->
+		</ssh>
 
-    <!-- add as many as u want -->
+		<!-- add as many as u want -->
 
-  </shortcuts>
+	</shortcuts>
 
-  <search>https://www.duckduckgoog.com/</search> <!-- search engine url -->
+	<search>https://www.duckduckgoog.com/</search> <!-- search engine url -->
 
 </config>
 ```
--- main.js
+++ main.js
@@ -3,8 +3,8 @@ var config;
 var timezone = "HKT"
 let date = new Date().getDate();
 var day,
-  month,
-  year = null;
+	month,
+	year = null;
 let dayArray = new Array(7);
 dayArray[0] = "Sunday";
 dayArray[1] = "Monday";
@@ -29,98 +29,98 @@ monthArray[11] = "December";
 
 // xml config
 function loadXMLDoc() {
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      config = this.responseXML;
-      clockConfig();
-      grouping();
-      searchEngine();
-    }
-  };
-  xmlhttp.open("GET", "config.xml", true);
-  xmlhttp.send();
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200) {
+			config = this.responseXML;
+			clockConfig();
+			grouping();
+			searchEngine();
+		}
+	};
+	xmlhttp.open("GET", "config.xml", true);
+	xmlhttp.send();
 }
 
 // clock
 function clockConfig(){
-  timezone = config.getElementsByTagName("timezone")[0].childNodes[0].nodeValue;
-  document.getElementById("clock-url").href = config.getElementsByTagName("clock")[0].getElementsByTagName("url")[0].childNodes[0].nodeValue;
+	timezone = config.getElementsByTagName("timezone")[0].childNodes[0].nodeValue;
+	document.getElementById("clock-url").href = config.getElementsByTagName("clock")[0].getElementsByTagName("url")[0].childNodes[0].nodeValue;
 
 }
 function displayDate() {
-  now = new Date();
-  if (date != now.getDate() || day == null) {
-    day = dayArray[now.getDay()];
-    month = monthArray[now.getMonth()];
-    date = now.getDate();
-    year = now.getFullYear();
-  }
-  ReactDOM.render(
-    day +
-      ", " +
-      month +
-      " " +
-      date +
-      ", " +
-      year +
-      " " +
-      (now.getHours() < 10 ? "0" + now.getHours() : now.getHours()) +
-      ":" +
-      (now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes()) +
-      ":" +
-      (now.getSeconds() < 10 ? "0" + now.getSeconds() : now.getSeconds()) +
-      " " +
-      timezone,
-    document.getElementById("clock")
-  );
+	now = new Date();
+	if (date != now.getDate() || day == null) {
+		day = dayArray[now.getDay()];
+		month = monthArray[now.getMonth()];
+		date = now.getDate();
+		year = now.getFullYear();
+	}
+	ReactDOM.render(
+		day +
+			", " +
+			month +
+			" " +
+			date +
+			", " +
+			year +
+			" " +
+			(now.getHours() < 10 ? "0" + now.getHours() : now.getHours()) +
+			":" +
+			(now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes()) +
+			":" +
+			(now.getSeconds() < 10 ? "0" + now.getSeconds() : now.getSeconds()) +
+			" " +
+			timezone,
+		document.getElementById("clock")
+	);
 
 }
 
 // favicon
 function change_favicon(img) {
-  var favicon = document.querySelector('link[rel="shortcut icon"]');
+	var favicon = document.querySelector('link[rel="shortcut icon"]');
 
-  if (!favicon) {
-    favicon = document.createElement("link");
-    favicon.setAttribute("rel", "shortcut icon");
-    var head = document.querySelector("head");
-    head.appendChild(favicon);
-  }
+	if (!favicon) {
+		favicon = document.createElement("link");
+		favicon.setAttribute("rel", "shortcut icon");
+		var head = document.querySelector("head");
+		head.appendChild(favicon);
+	}
 
-  favicon.setAttribute("type", "image/png");
-  favicon.setAttribute("href", img);
+	favicon.setAttribute("type", "image/png");
+	favicon.setAttribute("href", img);
 }
 
 // shortcuts
 function grouping() {
-  var x = config.getElementsByTagName("shortcuts")[0].childNodes;
-  var tags = [];
-  for (var i = 0; i < x.length; i++) {
-    if (x[i].nodeType != 3 && !tags.includes(x[i].nodeName)) {
-      display(x[i].nodeName);
-      tags.push(x[i].nodeName);
-    }
-  }
+	var x = config.getElementsByTagName("shortcuts")[0].childNodes;
+	var tags = [];
+	for (var i = 0; i < x.length; i++) {
+		if (x[i].nodeType != 3 && !tags.includes(x[i].nodeName)) {
+			display(x[i].nodeName);
+			tags.push(x[i].nodeName);
+		}
+	}
 }
 function display(Tag) {
-  var x = config.getElementsByTagName(Tag);
-  var content = "";
-  for (var i = 0; i < x.length; i++) {
-    content +=
-      '<td><a href="' +
-      x[i].getElementsByTagName("url")[0].childNodes[0].nodeValue +
-      '">' +
-      x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue +
-      "</a><td>";
-  }
-  document.getElementById("ls").innerHTML +=
-    '<tr><th id="h">' + Tag + "</th>" + content + "</tr>";
+	var x = config.getElementsByTagName(Tag);
+	var content = "";
+	for (var i = 0; i < x.length; i++) {
+		content +=
+			'<td><a href="' +
+			x[i].getElementsByTagName("url")[0].childNodes[0].nodeValue +
+			'">' +
+			x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue +
+			"</a><td>";
+	}
+	document.getElementById("ls").innerHTML +=
+		'<tr><th id="h">' + Tag + "</th>" + content + "</tr>";
 }
 
 // Search engine
 function searchEngine(){
-  document.getElementById("search-bar").action = config.getElementsByTagName("search")[0].childNodes[0].nodeValue;
+	document.getElementById("search-bar").action = config.getElementsByTagName("search")[0].childNodes[0].nodeValue;
 }
 
 loadXMLDoc();
